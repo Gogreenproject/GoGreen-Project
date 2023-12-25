9234ed0cb5f3f07aaddb0560c261e5870944b943
Shahlo Suvonqulova <shahlosuvonqulova@gmail.com>
2023-12-25 08:58:55 -0500

    updated

--- main.tf
+++ main.tf
@@ -5,22 +5,29 @@ resource "aws_key_pair" "key" {
 
 resource "aws_instance" "Web_Tier" {
   for_each      = var.Web_Tier_ec2
-  ami           = "ami-0230bd60aa48260c6"
-  instance_type = "t4g.nano"
+  ami           = "ami-01450e8988a4e7f44"
+  instance_type = "t2.micro"
   key_name      = aws_key_pair.key.key_name
 
   subnet_id              = aws_subnet.public_subnets[each.key].id
   vpc_security_group_ids = [module.security-groups.security_group_id["Web_tier_sg"]]
   #vpc_security_group_ids = [module.security_groups.security_group_id["cloud_2023_sg"]] 
 
-  #   #   user_data              = <<-EOF
-  #   #                            #!/bin/bash
-  #   #                            sudo yum update -y
-  #   #                            sudo yum install -y httpd
-  #   #                            sudo systemctl start httpd.service
-  #   #                            sudo systemctl enable httpd.service
-  #   #                            sudo echo "<h1> HELLO from ${upper(each.key)}_SERVER </h1>" > /var/www/html/index.html                  
-  #   #                            EOF
+  user_data = <<-EOF
+                             #!/bin/bash
+                             sudo yum update -y
+                             sudo systemctl start httpd.service
+                             sudo systemctl enable httpd.service
+                             sudo echo "<h1> HELLO from ${upper(each.key)}_SERVER </h1>" > /var/www/html/index.html    
+                             sudo yum -y install httpd php mysql php-mysql
+                             sudo chkconfig httpd on
+                             sudo if [ ! -f /var/www/html/lab-app.tgz ]; then
+                             sudo cd /var/www/html
+                             sudo wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/CUR-TF-200-ACACAD/studentdownload/lab-app.tgz
+                             sudo tar xvfz lab-app.tgz
+                             sudo chown apache:root /var/www/html/rds.conf.php
+                             sudo fi              
+                            EOF
   tags = {
     Name = join("_", [var.prefix, each.key])
   }
@@ -28,7 +35,7 @@ resource "aws_instance" "Web_Tier" {
 
 resource "aws_instance" "App_Tier" {
   for_each      = var.App_Tier_ec2
-  ami           = "ami-0230bd60aa48260c6"
+  ami           = "ami-01450e8988a4e7f44"
   instance_type = "t2.small"
   key_name      = aws_key_pair.key.key_name
 
@@ -50,7 +57,7 @@ resource "aws_instance" "App_Tier" {
 
 resource "aws_instance" "ec2_instance" {
   for_each      = var.ec2_instance
-  ami           = "ami-0230bd60aa48260c6"
+  ami           = "ami-01450e8988a4e7f44"
   instance_type = "t2.small"
   key_name      = aws_key_pair.key.key_name
 
