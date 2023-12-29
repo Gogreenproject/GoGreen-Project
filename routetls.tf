f7db7c5be7ae50be6ab455d99668e89c3f278f3d
Shahlo Suvonqulova <shahlosuvonqulova@gamil.com>
2023-12-28 19:11:22 -0500

    updated

--- routetls.tf
+++ routetls.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "rt_public_BS" {
+resource "aws_route_table" "rt_public" {
   vpc_id = module.vpc.vpc_id
 
   route {
@@ -11,29 +11,37 @@ resource "aws_route_table" "rt_public_BS" {
   }
 }
 
-resource "aws_route_table_association" "rta_public" {
-  for_each       = var.public_subnets
-  subnet_id      = aws_subnet.public_subnets[each.key].id
-  route_table_id = aws_route_table.rt_public_BS.id
-}
-
-resource "aws_route_table_association" "rta_private_at" {
-  for_each       = var.private_subnets_AT
-  subnet_id      = aws_subnet.private_subnets_AT[each.key].id
-  route_table_id = aws_route_table.rt_private_at[each.key].id
-}
-
-resource "aws_route_table" "rt_private_at" {
+resource "aws_route_table" "rt_private" {
   vpc_id = module.vpc.vpc_id
   for_each = var.public_subnets
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[each.key].id
+    nat_gateway_id = aws_nat_gateway.nat[each.key].id
   }
 
   tags = {
-    Name = "${var.prefix}-rtb-private_at"
+    Name = "${var.prefix}-rtb-private"
+  }
+}
+
+resource "aws_route_table_association" "rta_public" {
+  for_each       = var.public_subnets
+  subnet_id      = aws_subnet.public_subnets[each.key].id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "rta_private" {
+   for_each = local.private_mapping
+   subnet_id      = aws_subnet.private_subnets_AT[each.value].id
+   route_table_id = aws_route_table.rt_private[each.key].id
+}
+
+
+locals {
+  private_mapping = {
+    "app_tier_1" = "web_tier_1",
+    "app_tier_2" = "web_tier_2"
   }
 }
 
